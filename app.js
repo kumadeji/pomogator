ad69109d41707e7c9d86b66d1511969947bd2829
Cassian Kahema <kahemacassian@gmail.com>
2022-12-24 11:15:46 +1030

    input processing

---
+++ app.js
@@ -0,0 +1,15 @@
+const textArea = document.getElementById("text");
+var wordCount = 0;
+var charCount = 0;
+textArea.oninput = () => updateCounts();
+
+function updateCounts() {
+  // Split the text by spaces and filter out empty strings
+  let words = textArea.value
+    .split(" ")
+    .filter((word) => word.length > 0 && RegExp(/[a-z]+/i).test(word));
+  wordCount = words.length;
+  charCount = textArea.value.length;
+
+  console.log(wordCount, charCount);
+}
