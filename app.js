c6dd4f0dcb5232e2a741ba1c6c425caefd33342d
Alexey Vladimirov <kumadeji@yandex.ru>
2024-06-28 01:20:46 +0300

    test

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function updateCounts() {
   // Split the text by spaces and filter out empty strings
   let words = textArea.value
     .split(" ")
-    .filter((word) => word.length > 0 && RegExp(/[a-z]+/i).test(word));
+    .filter((word) => word.length > 0 && RegExp(/[\p{Alpha}\p{M}\p{Nd}\p{Pc}\p{Join_C}]/gu).test(word));
   wordCount = words.length;
   charCount = textArea.value.length;
 
@@ -15,3 +15,29 @@ function updateCounts() {
   document.documentElement.style.setProperty("--word-count", wordCount);
   document.documentElement.style.setProperty("--char-count", charCount);
 }
+
+// Удаление сносок
+$('a#replace').click(function(e) {
+    e.preventDefault();
+    updateCounts();
+    
+    var value = $('textarea[name="text"]').val();
+    value = value.replace(/\s*\[[^\]]*\]/g, '');
+    $('textarea[name="text"]').val(value);
+});
+
+// Функция для копирования текста
+function copyTextToClipboard(text) {
+    const textArea2 = document.createElement('textarea');
+    textArea2.value = text;
+    document.body.appendChild(textArea2);
+    textArea2.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea2);
+}
+
+// Добавляем обработчик события клика к кнопке
+document.getElementById('replace').addEventListener('click', function() {
+    updateCounts();
+    copyTextToClipboard(wordCount);
+});
